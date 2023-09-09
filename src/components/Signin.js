5ea7c454308c72b20fd4f0365b48680599ce855a
charan047 <93238151+charan047@users.noreply.github.com>
2023-09-09 20:47:24 +0530

    Update Signin.js

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import './Signin.css';
-// import API_BASE_URL from './apiConfig';
-const Signin = () => {
+
+const Signin = ({ closePopup })  => {
   const [formData, setFormData] = useState({
     userType: 'Student',
     username: '',
@@ -33,85 +33,119 @@ const Signin = () => {
           password: formData.password,
         }),
       });
-    
-      if (response.ok) { 
-        const token = await response.text();  
-    
+
+      if (response.ok) {
+        const token = await response.text();
+
         if (token) {
           localStorage.setItem('authToken', token);
           history.push('/feedspage');
+          closePopup();
         } else {
           console.error('No token found in response:', token);
           alert('Authentication failed. Please try again.');
         }
-      } else { 
+      } else {
         console.error('Authentication failed with status:', response.status);
         alert('Invalid credentials. Please try again.');
       }
     } catch (error) {
       console.error('An error occurred:', error);
     }
-    
   };
-    
 
   return (
-    <div className="signin-container">
-      <div className="form-box">
-        <h2>Sign In</h2>
-        <form onSubmit={handleSubmit}>
-          <div className="form-group">
-            <label htmlFor="userType">User Type:</label>
-            <select
-              id="userType"
-              name="userType"
-              value={formData.userType}
-              onChange={handleChange}
+    <div className="container mt-5">
+      <div className="container auth-container" style={{ maxWidth: '900px' }}>
+        <div className="row d-flex justify-content-center align-items-center h-100">
+          <div className="col-md-9 col-lg-6 col-xl-5">
+            <img
+              src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
+              className="img-fluid"
+              alt="Sample image"
+            />
+          </div>
+          <div className="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+            <form
+              style={{ maxWidth: '400px', margin: '0 auto' }}
+              onSubmit={handleSubmit} // Add onSubmit event handler
             >
-              <option value="Admin">Admin</option>
-              <option value="Student/Faculty">Student/Faculty</option>
-              {/* <option value="Guest">Guest</option> */}
-            </select>
-          </div>
-          <div className="form-group">
-          <label htmlFor="username">Username:</label>
-            <input
-              type="text"
-              id="username"
-              name="username"
-              value={formData.username}
-              onChange={handleChange}
-              required
-            />
+              {/* Username input */}
+              <div className="form-outline mb-4">
+                <label className="form-label" htmlFor="form3Example3">
+                  Username
+                </label>
+                <input
+                  type="text"
+                  id="form3Example3"
+                  className="form-control form-control-lg"
+                  placeholder="Enter a valid username"
+                  name="username" // Add name attribute
+                  value={formData.username} // Connect to state
+                  onChange={handleChange} // Add onChange event handler
+                />
+              </div>
 
+              {/* Password input */}
+              <div className="form-outline mb-3">
+                <label className="form-label" htmlFor="form3Example4">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  id="form3Example4"
+                  className="form-control form-control-lg"
+                  placeholder="Enter password"
+                  name="password" // Add name attribute
+                  value={formData.password} // Connect to state
+                  onChange={handleChange} // Add onChange event handler
+                />
+              </div>
+
+              <div className="d-flex justify-content-between align-items-center">
+                {/* Checkbox */}
+                <div className="form-check mb-0">
+                  <input
+                    className="form-check-input me-2"
+                    type="checkbox"
+                    value=""
+                    id="form2Example3"
+                  />
+                  <label className="form-check-label" htmlFor="form2Example3">
+                    Remember me
+                  </label>
+                </div>
+                <a href="#!" className="text-body">
+                  Forgot password?
+                </a>
+              </div>
+
+              <div className="text-center text-lg-start mt-4 pt-2">
+                <button
+                  type="submit"
+                  className="btn btn-primary btn-lg"
+                  style={{ paddingLeft: '2.5rem', paddingRight: '2.5rem' }}
+                >
+                  Login
+                </button>
+                <p className="small fw-bold mt-2 pt-1 mb-0">
+                  Don't have an account?{' '}
+                  <a href="/signup" className="link-danger">
+                    Register
+                  </a>
+                </p>
+              </div>
+            </form>
           </div>
-          <div className="form-group">
-            <label htmlFor="password">Password:</label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              value={formData.password}
-              onChange={handleChange}
-              required
-            />
-          </div>
-          <div className="button-container">
-            <button type="submit">Sign In</button>
-          </div>
-        </form>
-        <p>
-          Not a user? <a href="/signup">Sign Up</a>
-        </p>
-        <p>
-          Forget password? <a href="/reset-password">Reset Password</a>
-        </p>
+        </div>
+      </div>
+      <div className="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5 bg-primary">
+        <div className="text-white mb-3 mb-md-0">
+          Copyright © 2023. All rights reserved.
+        </div>
       </div>
     </div>
   );
 };
 
 export default Signin;
-
-
- 
